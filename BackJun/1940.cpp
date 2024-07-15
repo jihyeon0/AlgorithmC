c5584f75e7d4ab018049badac59ed429b0faf5b6
Jihyeon0 <wjswlqhd1@naver.com>
2024-07-15 20:29:50 +0900

    투 포인터 2문제

---
+++ BackJun/1940.cpp
@@ -0,0 +1,56 @@
+﻿#define EASYINPUT ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+/* a + b = M 이 되는 조합 수, 투포인터 */
+
+int main(void)
+{
+	EASYINPUT;
+
+	int N; //재료의 개수
+	int M; //갑옷을 만드는데 필요한 수
+	int cnt = 0;
+	vector<int> v;
+
+	cin >> N;
+	cin >> M;
+	v.resize(N);
+	
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int startindex = 0;
+	int endindex = N-1;
+
+	while (startindex < endindex)
+	{
+		if (v[startindex] + v[endindex] == M)
+		{
+			cnt++;
+			endindex--;
+			startindex++;
+			//v[startindex] + v[endindex] == M 이미 짝을 이루었으므로
+			// 현재 v[startindex] 또는 v[endindex] 로 나올 수 있는 경우의 수가 없음
+			// 2+b =7 에서 b =5 뿐임 그러므로 두 개 다 이동
+		}
+		else if (v[startindex] + v[endindex] < M)
+		{
+			startindex++;
+		}
+		else
+		{
+			endindex--;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
---
+++ BackJun/2018.cpp
@@ -0,0 +1,59 @@
+﻿#define EASYINPUT ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#include <iostream>
+
+using namespace std;
+/* 투포인터 */
+
+int main(void)
+{
+	EASYINPUT;
+
+	int N;
+	int startindex = 1;
+	int endindex = 1;
+	int sum = 1;
+	int cnt = 1;
+
+	cin >> N;
+
+	//while (endindex != N)
+	//{
+	//	if (sum < N)
+	//	{
+	//		sum = sum + endindex; endindex++; 
+	//		//sum = 0 으로 시작 , endindex++ 가 뒤에 있음
+	//		// 3인 경우 1+2 하고 바로 탈출해서 cnt 2가 아니라 1이됨
+	//	}
+	//	else if (sum > N)
+	//	{
+	//		sum -= startindex; startindex++;
+	//	}
+	//	else if (sum == N)
+	//	{
+	//		cnt++; 
+	//		sum -= startindex; startindex++;
+	//	}
+	//}
+
+	while (endindex != N)
+	{
+		if (sum == N)
+		{
+			cnt++;
+			sum -= startindex; startindex++;
+		}
+		else if (sum < N)
+		{
+			endindex++;
+			sum += endindex;
+		}
+		else
+		{
+			sum -= startindex; startindex++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
