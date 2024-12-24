f4cedacf88193c16398375e93ec443ce2ab7ff73
jihyeon0 <113276954+jihyeon0@users.noreply.github.com>
2024-12-24 11:18:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 2.83 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+long long answer = 0;
+long long x = 1;
+    while(x * x <= n)
+    {
+        if(x * x == n) 
+        {
+            answer = (x+1)*(x+1);
+            return answer;
+        }
+        x++;
+    }
+    answer = -1;
+    return answer;
+}
