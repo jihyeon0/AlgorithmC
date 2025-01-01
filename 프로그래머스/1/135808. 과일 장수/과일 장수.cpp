c6433f9b77d1831bee6084c4022a63d0da521caa
jihyeon0 <113276954+jihyeon0@users.noreply.github.com>
2025-01-01 19:01:13 +0900

    [level 1] Title: 과일 장수, Time: 46.69 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), greater<int>());
+    
+    for(int i=0; i<score.size()%m; i++)
+    {
+        score.pop_back();
+    }
+    
+    for(int i=1; i<=score.size()/m; i++)
+    {
+        answer += score[i*m-1]*m;
+    }
+    return answer;
+}
