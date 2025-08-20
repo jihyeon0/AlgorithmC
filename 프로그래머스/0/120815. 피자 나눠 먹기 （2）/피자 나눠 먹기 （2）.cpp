f9a745b0f51be21b3d209d59c08b7708ba4554f2
Jihyeon Jeon <113276954+jihyeon0@users.noreply.github.com>
2025-08-20 19:21:41 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+
+    while ((6 * answer) % n != 0)
+    {
+        answer++;
+    }
+    return answer;
+}
